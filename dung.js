3539108161719e458342d12d3d19629e2a4e0aa4
rose <Becausedloveh@gmail.com>
2018-06-27 20:13:28 +0700

    page

---
+++ dung.js
@@ -0,0 +1,8 @@
+function myinf() {
+    var x = document.getElementById("demo");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
